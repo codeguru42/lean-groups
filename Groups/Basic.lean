0a9c0e7bf8307b04b592637bf6f4767a1b54d00c
Code-Apprentice <codeguru42@gmail.com>
2025-11-29 18:14:02 -0700

    Add definition for a group

--- Groups/Basic.lean
+++ Groups/Basic.lean
@@ -1,1 +1,8 @@
-def hello := "world"
+structure Group (α : Type) where
+  (G : Type)
+  (mul : G → G → G)
+  (e : G)
+  (inv : G → G)
+  (mul_assoc : ∀ a b c : G, mul (mul a b) c = mul a (mul b c))
+  (mul_e : ∀ a : G, mul a e = a ∧ mul e a = a)
+  (mul_inv : ∀ a : G, mul a (inv a) = e ∧ mul (inv a) a = e)
