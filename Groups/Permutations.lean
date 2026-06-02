74b91c3c3a716995fd144a909742cab924808853
codeguru42 <codeguru42@gmail.com>
2026-06-01 20:01:45 -0600

    Start proof for cancellation law for permutations

--- Groups/Permutations.lean
+++ Groups/Permutations.lean
@@ -6,4 +6,12 @@ open Functions
 
 def permutation (f : α -> α) := bijective f
 
+theorem cancellation :
+  permutation α ∧ permutation β ∧ permutation γ ∧
+    ((α ∘ β = α ∘ γ) ∨ (β ∘ α = γ ∘ α)) → β = γ := by
+  intro ⟨ha, hb, hc, h⟩
+  cases h
+  · sorry
+  · sorry
+
 end Permutations
