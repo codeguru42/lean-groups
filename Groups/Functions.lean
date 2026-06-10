ea24b375f5f705c29838adb626962b641db2d7c3
Code-Apprentice <codeguru42@gmail.com>
2026-06-09 21:14:18 -0600

    For now we only has_inverse_implies_bijection

--- Groups/Functions.lean
+++ Groups/Functions.lean
@@ -6,28 +6,24 @@ def bijective (f : α → β) := injective f ∧ surjective f
 def ident : α -> α := fun x => x
 def has_inverse (f : α → β) := ∃g : β -> α, f ∘ g = ident ∧ g ∘ f = ident
 
-theorem bijection_iff_has_inverse :
-  (∀ f: α → β, bijective f ↔ has_inverse f) := by
-  intro f
-  apply Iff.intro
-  · intro ⟨hi, hs⟩
-    sorry
-  · intro hi
-    apply And.intro
-    · intro x₁ x₂ hf
-      cases hi with
-      | intro w hw =>
-        have h' : (w ∘ f) x₁ = (w ∘ f) x₂ := by
-          change w (f x₁) = w (f x₂)
-          rw [hf]
-        rw [hw.2] at h'
-        exact h'
-    · intro y
-      cases hi with
-      | intro w hw =>
-        · exists w y
-          change (f ∘ w) y = y
-          rw [hw.1]
-          rfl
+theorem has_inverse_implies_bijection :
+  (∀ f: α → β, has_inverse f → bijective f) := by
+  intro f hi
+  apply And.intro
+  · intro x₁ x₂ hf
+    cases hi with
+    | intro w hw =>
+      have h' : (w ∘ f) x₁ = (w ∘ f) x₂ := by
+        change w (f x₁) = w (f x₂)
+        rw [hf]
+      rw [hw.2] at h'
+      exact h'
+  · intro y
+    cases hi with
+    | intro w hw =>
+      · exists w y
+        change (f ∘ w) y = y
+        rw [hw.1]
+        rfl
 
 end Functions
